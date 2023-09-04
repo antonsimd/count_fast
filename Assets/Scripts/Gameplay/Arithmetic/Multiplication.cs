859d96a6212286f84fa5063ac057b7eab7b85f5f
Anton Simdyankin <antonsimd@gmail.com>
2023-09-04 13:10:05 +1000

    updated multiplication difficulty

--- Assets/Scripts/Gameplay/Arithmetic/Multiplication.cs
+++ Assets/Scripts/Gameplay/Arithmetic/Multiplication.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Multiplication : MonoBehaviour
 {
+    const int multiplicationRangeFactor = 5;
+
     static System.Random random = new System.Random();
 
     public static int generateQuestion() {
-        int number1 = random.Next(MainGame.mainGame.range.Item1, MainGame.mainGame.range.Item2);
-        int number2 = random.Next(MainGame.mainGame.range.Item1, MainGame.mainGame.range.Item2);
+        int number1 = random.Next(MainGame.mainGame.range.Item1, MainGame.mainGame.range.Item2 / multiplicationRangeFactor);
+        int number2 = random.Next(MainGame.mainGame.range.Item1, MainGame.mainGame.range.Item2 / multiplicationRangeFactor);
 
-        MainGame.mainGame.questionText.text = number1.ToString() + " x " + number2.ToString();
+        MainGame.mainGame.questionText.text = number1.ToString() + "* " + number2.ToString();
 
         return number1 * number2;
     }
