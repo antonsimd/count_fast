a4beba6be1641c3379294d8876e61664f34f4c30
Anton Simdyankin <antonsimd@gmail.com>
2023-10-05 14:43:29 +1100

    confirm on element removal

--- Assets/Scripts/SaveData/TableRow.cs
+++ Assets/Scripts/SaveData/TableRow.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class TableRow : MonoBehaviour
 {
+    const string REMOVE_ELEMENT_TEXT = "remove saved game";
+
+    [SerializeField] GameObject confirmPopupPrefab;
+
     int index;
 
     public void setIndex(int i) {
@@ -13,5 +17,9 @@ public class TableRow : MonoBehaviour
     public void removeElement() {
         DataManager.removeAtIndex(index);
         LoadTable.instance.resetScoreboard();
+    }
+
+    public void confirmRemoveElement() {
+        ConfirmPopup.createPopup(confirmPopupPrefab, REMOVE_ELEMENT_TEXT, removeElement);
     }
 }
