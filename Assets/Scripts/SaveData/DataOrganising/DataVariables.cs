87d85252eaaca2f635b95c8582f22ee22610a470
Anton Simdyankin <antonsimd@gmail.com>
2023-10-20 13:35:18 +1100

    added type of the game to sort functions

---
+++ Assets/Scripts/SaveData/DataOrganising/DataVariables.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DataVariables
+{
+    public enum SortTypes {
+        ScoreUp,
+        ScoreDown,
+        DateUp,
+        DateDown,
+        TimeUp,
+        TimeDown,
+        NameUp,
+        NameDown
+    };
+}
--- Assets/Scripts/SaveData/DataOrganising/SortData.cs
+++ Assets/Scripts/SaveData/DataOrganising/SortData.cs
@@ -3,41 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+using SortType = DataVariables.SortTypes;
+
 public class SortData : MonoBehaviour
 {
     public void sortAcsendingByScore() {
-        sortArray(new SortScoreAscending());
+        sortArray(new SortScoreAscending(), SortType.ScoreUp);
     }
 
     public void sortDescendingByScore() {
-        sortArray(new SortScoreDescending());
+        sortArray(new SortScoreDescending(), SortType.ScoreDown);
     }
 
     public void sortDescendingByDate() {
-        sortArray(new SortDateDescending());
+        sortArray(new SortDateDescending(), SortType.DateDown);
     }
 
     public void sortAscendingByDate() {
-        sortArray(new SortDateAscending());
+        sortArray(new SortDateAscending(), SortType.DateUp);
     }
 
     public void sortDescendingByTime() {
-        sortArray(new SortTimeDescending());
+        sortArray(new SortTimeDescending(), SortType.TimeDown);
     }
 
     public void sortAscendingByTime() {
-        sortArray(new SortTimeAscending());
+        sortArray(new SortTimeAscending(), SortType.TimeUp);
     }
 
     public void sortDecsendingByName() {
-        sortArray(new SortNameDescending());
+        sortArray(new SortNameDescending(), SortType.NameDown);
     }
 
     public void sortAcsendingByName() {
-        sortArray(new SortNameAscending());
+        sortArray(new SortNameAscending(), SortType.NameUp);
     }
 
-    void sortArray(IComparer comparer) {
+    void sortArray(IComparer comparer, SortType type) {
         SaveData data = DataManager.getSaveData();
 
         // Sort array by the given comparer, save the result to JSON
