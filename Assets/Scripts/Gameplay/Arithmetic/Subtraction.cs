0051be357829b011378e82b0209b077e00e4dc0a
Anton Simdyankin <antonsimd@gmail.com>
2023-10-21 19:05:46 +1100

    include or exclude negatives in subtraction

--- Assets/Scripts/Gameplay/Arithmetic/Subtraction.cs
+++ Assets/Scripts/Gameplay/Arithmetic/Subtraction.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class Subtraction : MonoBehaviour
 {
     static System.Random random = new System.Random();
+    static bool includeNegatives = false;
 
     public static int generateQuestion() {
         int number1 = random.Next(Difficulty.range.Item1, Difficulty.range.Item2);
         int number2 = random.Next(Difficulty.range.Item1, Difficulty.range.Item2);
 
-        MainGame.mainGame.questionText.text = number1.ToString() + " - " + number2.ToString();
+        if (!includeNegatives && number1 < number2) {
+            MainGame.mainGame.questionText.text = number2.ToString() + " - " + number1.ToString();
+            return number2 - number1;
+        } else {
+            MainGame.mainGame.questionText.text = number1.ToString() + " - " + number2.ToString();
 
-        return number1 - number2;
+            return number1 - number2;
+        }
+    }
+
+    public static void setNegatives(bool x) {
+        includeNegatives = x;
     }
 }
